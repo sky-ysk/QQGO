8ccafe1a64ce69183cd5c2487b9501aedd1dfa20
sky-ysk <sky-ysk@qq.com>
2026-06-03 19:55:13 +0800

    feat(desktop): initialize Wails v2 project structure

---
+++ cmd/desktop/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:     "QQGO",
+		Width:     960,
+		Height:    640,
+		MinWidth:  720,
+		MinHeight: 480,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
